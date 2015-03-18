89cfd938eff210b21f4fc08332d85f7266bbef51
Andrea Giardini <contact@andreagiardini.com>
2015-03-18 14:20:10 +0100

    Allow import tpg

---
+++ tpg/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+""Geneve TPG API to get informations about trams/buses""
+__version__ = "0.1"
+__author__ = "Andrea Giardini ( http://www.andreagiardini.com/ )"
+__author_email__ = "contact@andreagiardini.com"
+__license__ = "See: http://creativecommons.org/licenses/by-nd/3.0/ "
+from .Client import Clieny
